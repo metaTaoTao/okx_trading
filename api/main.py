417b1934581586dd2f5588cfd634923ebdc48683
uncczhangtao <uncczhangtao@gmail.com>
2025-06-10 20:00:06 -0400

    add health function

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from api.routes.kline import kline_bp
 
 app = Flask(__name__)
 app.register_blueprint(kline_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+@app.route("/health")
+def health_check():
+    return jsonify({"status": "ok"})
